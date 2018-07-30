278dcc50192295a9bad0c8da924f522b345336a1
zhoutao0712 <846207054@qq.com>
2018-07-30 17:36:49 +0800

    random myport range 6500-6599

--- trunk/user/tinc/tinc-1.16pre/src/net_setup.c
+++ trunk/user/tinc/tinc-1.16pre/src/net_setup.c
@@ -825,6 +825,13 @@ void device_disable(void) {
 	}
 }
 
+static char *get_rand_port(int min, int max) {
+	char buf[64];
+	int port_num = rand()%(max - min +1) + min;
+	sprintf(buf, "%d", port_num);
+	return xstrdup(buf);
+}
+
 /*
   Configure node_t myself and set up the local sockets (listen only)
 */
@@ -847,9 +854,12 @@ static bool setup_myself(void) {
 
 	if(!get_config_string(lookup_config(config_tree, "Port"), &myport)) {
 		myport = xstrdup("655");
+		myport = get_rand_port(6500, 6599);
 	} else {
 		port_specified = true;
 	}
+
+	logger(DEBUG_ALWAYS, LOG_ERR, "myport=%s", myport);
 
 	myself->connection->options = 0;
 	myself->connection->protocol_major = PROT_MAJOR;
